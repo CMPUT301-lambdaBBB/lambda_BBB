ed96738449524433071c42b15b99c2d410da1118
Dehai Yu <dehai1@ualberta.ca>
2026-09-23 19:10:52 -0600

    Added colour to circle

--- lambda_BBB/app/src/main/java/com/example/lambda_bbb/Circle.kt
+++ lambda_BBB/app/src/main/java/com/example/lambda_bbb/Circle.kt
@@ -1,7 +1,9 @@
 package com.example.lambda_bbb
 
 class Circle(x:Int, y:Int):Shape() {
-    override var x = x
-    override var y = y
+    override var x = 0
+    override var y = 0
+
+    val color: String = "Green"
 
 }
